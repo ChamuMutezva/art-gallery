44bd812c9027b638630a029959b41acad2cf13bb
Chamunorwa Mutezva <ckmutezva@gmail.com>
2021-12-16 08:37:40 +0200

    feat: scrolltrigger animations

--- src/pages/layouts/Header.js
+++ src/pages/layouts/Header.js
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import heroMobile from '../../assets/mobile/image-hero.jpg'
 import heroMobile2 from '../../assets/mobile/image-hero@2x.jpg'
 import heroTablet from '../../assets/tablet/image-hero.jpg'
@@ -8,6 +11,17 @@ import arrowRight from '../../assets/icon-arrow-right.svg'
 import LinkBtn from "../../components/LinkBtn";
 
 function Header() {
+const h1Ref = useRef(null)
+    useEffect(() => {
+        gsap.from(h1Ref.current, {
+            duration: 1, 
+            autoAlpha: 0,
+            ease: "none",
+            delay: 1
+        })
+
+    })
+    
     return (
         <header className="header">
           
@@ -20,7 +34,7 @@ function Header() {
                     </picture>
                 </div>
                 <div className="container main-content">
-                    <h1 className="main-heading headings heading-h1 uppercase">Modern Art Gallery</h1>
+                    <h1 ref={h1Ref} className="main-heading headings heading-h1 uppercase">Modern Art Gallery</h1>
                     <p className="main-content-text">
                         The arts in the collection of the Modern Art Gallery all started
                         from a spark of inspiration. Will these pieces inspire you? Visit
--- src/pages/layouts/Main.js
+++ src/pages/layouts/Main.js
@@ -1,12 +1,50 @@
-import { useContext } from "react"
+import { useContext, useRef, useEffect } from "react"
+import { gsap } from "gsap"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { DataContext } from "../../context/DataContext"
 
 function Main() {
+
+    gsap.registerPlugin(ScrollTrigger)
+
     const { data } = useContext(DataContext)
-    console.log(data)
+    const pictureRef = useRef([])    
+    pictureRef.current = []
+
+    const addToRefs = (el) => {
+        if (el && !pictureRef.current.includes(el)) {
+            pictureRef.current.push(el)
+        }
+
+    }
+
+    useEffect(() => {
+        pictureRef.current.forEach((el, index) => {
+            gsap.fromTo(el,
+                {
+                    autoAlpha: 0
+                },
+                {
+                    duration: 1,
+                    autoAlpha: 1,
+                    ease: "none",
+                    scrollTrigger: {
+                        id: `pic-${index+1}`,
+                        trigger: el,
+                        start: "top center+=100",
+                        toggleActions: "play none none reverse",
+                       // markers: true
+                    }
+                })
+        })
+
+        
+    })
+
+
     return (
         <main className="main container grid">
-            <div className="sub-content sub-content1">
+            <div className="sub-content sub-content1" ref={addToRefs}>
                 <div>
                     <h2 className="headings heading-h2 uppercase">Your day at the gallery</h2>
                     <p className="main-content-text">
@@ -16,7 +54,7 @@ function Main() {
                 </div>
             </div>
             {data.map(image => {
-                return <picture key={image.id} className={`${image.name}`}>
+                return <picture key={image.id} className={`${image.name}`} ref={addToRefs}>
                     <source media="(min-width: 1080px)" srcSet={`${image.desktop[0]}, ${image.desktop[1]} 2x`} />
                     <source media="(min-width: 620px)" srcSet={`${image.tablet[0]}, ${image.tablet[1]} 2x`} />
                     <img className="hero-img" src={`${image.mobile[0]}`} alt={image.content} srcSet={`${image.mobile[1]} 2x`} />
@@ -24,7 +62,7 @@ function Main() {
                 </picture>
             })}
 
-            <div className="sub-content sub-content2 black-bg">
+            <div className="sub-content sub-content2 black-bg" ref={addToRefs}>
                 <div className="container inner-container">
                     <h2 className="headings heading-h2 uppercase">Come &amp; be inspired</h2>
                     <p className="main-content-text">
